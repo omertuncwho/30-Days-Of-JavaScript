e5aa7949df9e4b53ae4562d6f817cdd125224e9b
Ömer Tunç <omertuncwho@gmail.com>
2024-01-07 14:44:14 +0300

    Day One Update

---
+++ 01-day/js/datatypes.js
@@ -0,0 +1,17 @@
+//checking types of variables
+console.log(typeof num);
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof num3);
+console.log(typeof num4);
+
+console.log(typeof firstName);
+console.log(typeof secondName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof issingle);
+
+console.log(typeof freezingPoint);
+console.log(typeof PI);
+console.log(atmPressure);
---
+++ 01-day/js/variables.js
@@ -0,0 +1,27 @@
+//declaring diffrent type of variables 
+
+var num = 12345;
+var num1 = 45.346;
+var num2 = -34.422;
+var num3="";
+var num4;
+
+let firstName = "John";
+let secondName = "Doe";
+let country = "Italy";
+let city = "Milano";
+let age = 45;
+let isSingle = false;
+
+let name = "John", 
+surname = "Doe", 
+country_now="Italy",
+city_now="Milano",
+age_now="45",
+isMarried=true;
+
+let myAge=25;
+let yourAge=23;
+
+ console.log("I am "+myAge+" years old.");
+ console.log("You are "+yourAge+ "years old");
