ed31c3583a55448aa8b6e12ed4bb78d103500d47
Ömer Tunç <omertuncwho@gmail.com>
2024-01-08 23:49:06 +0300

    update day 3

---
+++ 03-day/js/exercise_02.js
@@ -0,0 +1,24 @@
+/*let base= prompt('Enter base of the triangle');
+let height=prompt('Enter height of the triangle');
+let area =((0.5)*base*height);
+alert('The area of the triangle is '+ area);*/
+/*
+let side_a =5;
+let side_b=4;
+let side_c=3;
+let perimeter=(side_a+side_b+side_c);
+console.log('The perimeter of triangle is '+perimeter);*/
+
+/*
+let width=prompt('Enter width of the rectangle');
+let length=prompt('Enter length of the rectangle');
+let secondPerimeter =(2*(length+width));
+alert('Perimeter of triangle is'+secondPerimeter);*/
+/*
+let radius=prompt('Enter radius of circle');
+const pi=3.14;
+let circleArea=(pi*(radius*radius));
+let circumFerence=(2*pi*radius);
+alert('Area of the circle is '+circleArea);
+alert('Circumference of the circle is '+circumFerence);*/
+
