c1de64830abf398c354086d25f3abdba4172affd
Omer Tunc <omertuncwho@gmail.com>
2024-01-16 23:17:33 +0300

    Day 4 update

---
+++ 04-day/exercise_01.js
@@ -0,0 +1,53 @@
+/*let age = prompt('EnterYourAge',"Age");
+console.log(age);
+
+if(age>18)
+{
+console.log("You are old enough to drive");
+}
+else 
+{
+console.log(`You are ${age} years old. You are not old enough to drive!`);
+
+}*/
+/*
+let myAge=25;
+let yourAge=prompt(`Enter Your Age`,`Age PLS...`);
+if (yourAge<=0)
+{
+console.log(`You entered ${yourAge} please enter a vaild number.!`);
+}
+else if(myAge>yourAge)
+{
+console.log(`I am ${myAge} years old. I am ${myAge-yourAge} years older than you..`);
+}
+else if (myAge<=yourAge)
+{
+console.log(`I am ${myAge} years old. You are ${yourAge-myAge} year older than me.`);
+}
+else if(myAge==yourAge)
+{
+console.log(`You and I at the same age. We are ${myAge} years old.`);
+}
+else if (yourAge<=0)
+{
+console.log(`You entered ${yourAge} please enter a vaild number.!`);
+}
+else {
+console.log(`You entered ${yourAge} please enter a vaild number.!`);}
+*/
+/*let a = 4;
+let b=5;
+
+a>b? console.log(`${a} greater than ${b}.`) : console.log(`${b} greater than ${a}`);*/
+
+let number = prompt(`Is your number an even number?`, `Enter a number`);
+
+if((number%2) ===0)
+{
+alert(`${number} is an even number.`);
+}
+else 
+{
+alert(`${number} is an odd number.`);
+}
