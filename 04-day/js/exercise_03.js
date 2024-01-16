61a329dab52d44ef4e616d03b6214e2208d1183d
Omer Tunc <omertuncwho@gmail.com>
2024-01-17 01:22:29 +0300

    Day 04 Update

---
+++ 04-day/js/exercise_03.js
@@ -0,0 +1,45 @@
+let newMonth = "sEpTemBeR";
+let month = newMonth[0].toUpperCase() + newMonth.slice(1).toLowerCase();
+let l=31;
+let m=30;
+let s=28;
+
+if (month == `January`) {
+    console.log(`${month} is ${l} days.`);
+}
+else if (month == `February`) {
+    console.log(`${month} is ${s} days.`);
+}
+else if (month == `March`) {
+    console.log(`${month} is ${l} days.`);
+}
+else if (month == `April`) {
+    console.log(`${month} is ${m} days.`);
+}
+else if (month == `May`) {
+    console.log(`${month} is ${l} days.`);
+}
+else if (month == `June`) {
+    console.log(`${month} is ${m} days.`);
+}
+else if (month == `July`) {
+    console.log(`${month} is ${l} days.`);
+}
+else if (month == `August`) {
+    console.log(`${month} is ${l} days.`);
+}
+else if (month == `September`) {
+    console.log(`${month} is ${m} days.`);
+}
+else if (month == `October`) {
+    console.log(`${month} is ${l} days.`);
+}
+else if (month == `November`) {
+    console.log(`${month} is ${m} days.`);
+}
+else if (month == `December`) {
+    console.log(`${month} is ${l} days.`);
+}
+else {
+console.log(`enter a vaild month...!`)
+}
