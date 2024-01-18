8428a17be9e8caf0f0dff57c5f6de0716b68a294
Omer Tunc <omertuncwho@gmail.com>
2024-01-18 22:02:49 +0300

    Day 5 Update

---
+++ 05-day/js/app.js
@@ -0,0 +1,24 @@
+const fruits =[`apple`,`banana`,`strawberry`,`grape`,`coconut`];
+const numbers=[0,33,445,2123,42,51,14,13,4,1,3,2,33];
+const vegetables=[`Tomato`,`Potato`,`Cabbage`,`Onion`,`Carrot`];
+const animalProducts=[`milk`,`meat`,`yogurt`,`butter`]
+const countries=[`Finland`,`Denmark`,`Sweden`,`Norway`];
+
+console.log(numbers);
+console.log(`length of the numbers ${numbers.length}`);
+
+console.log(fruits);
+console.log(`length of the fruits ${fruits.length}`);
+
+console.log(vegetables);
+console.log(`Length of the vegetables ${vegetables}`);
+
+console.log(animalProducts);
+console.log(`Length of the products ${animalProducts.length}`);
+
+console.log(countries);
+console.log(`Length of the fruits ${countries.length}`);
+
+const mixedArray= [3,`Tomato`,4,`Eggs`,1,`Pan`,1,`Stove`];
+console.log(mixedArray);
+console.log(`Length of mixed array ${mixedArray.length}`);
