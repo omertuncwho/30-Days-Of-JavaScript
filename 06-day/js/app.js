9353e66683ad244fdc1e708ee7f33b9e15b2878b
Omer Tunc <omertuncwho@gmail.com>
2024-01-22 22:52:23 +0300

    Day 6 Update

---
+++ 06-day/js/app.js
@@ -0,0 +1,144 @@
+  /*for(initialization,contdition,increment/decrement)
+  {
+code
+}/*/
+/*
+for(let i = 0;i<=5;i++)
+{
+console.log(i);
+}
+console.log(`_______________________________`)
+for(let i=5;i>=0;i--)
+{
+console.log(i);
+}
+
+for (let i = 0; i <=10; i++)
+{
+console.log(`${i}*${i}=${i*i}`);
+}
+*/
+/*
+const countries =[`Finland`,`Sweden`,`Denmark`,`Norway`,`Iceland`,`ICEYLAND`];
+const newArray=[];
+for (let i = 0;i<countries.length;i++)
+newArray.push(countries[i].toUpperCase())
+console.log(newArray);
+const numbers = [1, 2, 3, 4, 5]
+let sum = 0
+for(let i = 0; i < numbers.length; i++){
+  sum  = sum + numbers[i]
+}
+console.log(sum);
+console.log(`______________________________`)
+
+*/
+/*
+const num =[1,2,3,4,5,6];
+console.log(num);
+const myArray = [];
+let sum=0;
+for(let i =0;i<num.length;i++)
+{
+myArray.push(num[i]**2);
+}
+console.log(myArray);*/
+/*
+const countries =[`FINLAND`,`SWEDEN`,`Denmark`,`NORWAY`,`Iceland`,`Germany`];
+const sorting =[];
+for(let i =0; i< countries.length;i++)
+{
+sorting.push(countries[i].toLowerCase());
+}
+console.log(sorting);
+
+let q = 0;
+while (q<=5)
+{
+console.log(q)
+q++
+}*/
+/*
+let w = 0;
+do{
+console.log(w);
+w++;
+}
+while(w<=5)
+
+*/
+/*
+const numar = [1,2,3,4,5,6,7,8];
+for(const numb of numar)
+{
+console.log(numb)} 
+
+console.log(`++++++++++++++++++++++++`)
+
+let sum=0;
+for(const num of numar)
+{
+sum=sum+sum;
+}
+console.log(sum);*//*
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ];
+
+  console.log(webTechs);
+for (const wtech of webTechs)
+{
+console.log(wtech.toUpperCase());
+}
+
+for (const tech of webTechs)  
+{
+console.log(tech[0]);
+}
+*/
+
+
+
+for(let p = 0; p <= 5; p++){
+    if(p == 3){
+      break
+    }
+    console.log(p)
+  }
+
+
+
+
+
+
+
+  for(let i = 0; i <= 5; i++){
+    if(i == 3){
+      continue
+    }
+    console.log(i)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 06-day/js/exercise_01.js
@@ -0,0 +1,145 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+]
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+/*
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+
+for (let k = 10; k >= 0; k--) {
+    console.log(k);
+}
+
+for ( let n=35; n<=0;n++)
+{
+console.log(n);
+}*/
+
+/*
+for (let i = 1; i <= 7; i++) {
+    let line = '';
+    for (let j = 1; j <= i; j++) {
+        line += '#';
+    }
+    console.log(line);
+}
+
+
+for(let x =0;x<=10;x++)
+{
+for(let xy=0;xy<=10;xy++)
+{
+console.log(`${x}x${x}=${x*x}`);
+}
+}
+
+
+
+
+for (let x = 0; x <= 10; x++) {
+    for (let xy = 0; xy <= 10; xy++) {
+        for (let xy = 0; xy <= 10; xy++) {
+            console.log(`${x} ${x^2}  ${x^3}`);
+        }
+        
+}}
+
+*/
+/*
+for (i=0;i<=100;i++)
+if(i%2===0)
+{
+console.log(i)}
+*/
+/*
+for (i=0;i<=100;i++)
+if(i%2===1)
+{
+console.log(i)}*/
+
+let total = 0;
+
+for (let i = 0; i <= 100; i++) {
+
+    total += i;
+}
+
+console.log(`he sum of all numbers from 0 to 100 is ${total}.`)
+
+
+
+let totalEvens = 0;
+let totalOdds = 0;
+
+for (let i = 0; i <= 100; i += 2) {
+
+    totalEvens += i;
+
+
+    totalOdds += i / 2;
+}
+
+console.log(`The sum of all evens from 0 to 100 is ${totalEvens}. And the sum of all odds from 0 to 100 is ${totalOdds}.
+`)
+
+let evenSum = 0;
+let ratioSum = 0;
+let resultArray = [];
+for (let number = 0; number <= 100; number += 2) {
+    evenSum += number;
+
+    ratioSum += number / 2;
+    resultArray.push({ number, evenSum, ratioSum });
+}
+console.log(resultArray);
+
+function generateRandomArray(length) {
+    const randomArray = [];
+    for (let i = 0; i < length; i++) {
+        const randomNumber = Math.floor(Math.random() * 100);
+        randomArray.push(randomNumber);
+    }
+    return randomArray;
+}
+
+const myRandomArray = generateRandomArray(5);
+
+console.log(`Random Arrays: ${myRandomArray}`);
+
+function generateRandomId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomId = '';
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomId += characters[randomIndex];
+    }
+    return randomId;
+}
+
+const myRandomId = generateRandomId();
+
+console.log("Random IDs, ${myRandomId}`);
