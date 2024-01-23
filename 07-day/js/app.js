ad9cc1944c51f1bc51e82b5c1fd9c626c9e35bb8
Ömer Tunç <omertuncwho@gmail.com>
2024-01-23 22:16:13 +0300

    Day 7 Update

---
+++ 07-day/js/app.js
@@ -0,0 +1,110 @@
+function square()
+{
+let sideOne=4;
+let sq=sideOne*sideOne;
+console.log(sq)
+}
+square();
+
+function topla(){
+let sayi1 =1900;
+let sayi2=23;
+let toplam=sayi1+sayi2;
+console.log(toplam);
+}
+topla();
+
+/*
+function printFullName(){
+let firstName=`Ömer`;
+let lastName=`Tunç`;
+let spacing=` `
+let fullName=(`My name is ${firstName}${spacing}${lastName}`)
+console.log(fullName)
+}
+printFullName();
+*/
+/*
+function printFullName(){
+    let firstName=`Ömer`;
+    let lastName=`Tunç`;
+    let spacing=` `
+    let fullName=(`My name is ${firstName}${spacing}${lastName}`)
+return fullName;
+    }
+
+    console.log(printFullName());
+*/
+    /*
+    function addTwoNumbers() {
+        let numOne = 2;
+        let numTwo = 3;
+        let total = numOne + numTwo;
+        return total;
+  
+    }
+  
+  console.log(addTwoNumbers())*/
+/*
+  function areaOfCircle(r){
+let area= Math.PI*r*r;
+return area;
+}
+console.log(areaOfCircle(10))
+
+
+function addThreeNumbers(numberOne,numberTwo,numberThree)
+{
+let sum = numberOne+numberTwo+numberThree;
+return sum;
+}
+console.log(addThreeNumbers(10,22,33));
+*/
+/*
+function concatName(fname,sname){
+return `${fname} ${sname}`;
+}
+console.log(concatName(`Luke`,`Skycoder`));*/
+/*
+function summingArrayValues(myArray){
+let sum = 0 ;
+for (let i = 0 ; i<myArray.length;i++)
+{
+sum=sum+myArray[i];
+}
+return sum;
+}
+const numbers=[1,2,3,4,5,6,8];
+console.log(summingArrayValues(numbers));
+
+function square(n)
+{
+return n*n;
+}
+console.log(square(2));
+
+*/
+
+const square = n => {
+
+return n*n ;
+}
+console.log(square(2));
+
+/*const square = n => n*n;*/
+
+
+const changeToUpperCase = arr => {
+    const newArr = []
+    for (const element of arr) {
+      newArr.push(element.toUpperCase());
+    }
+    return newArr;
+  }
+  
+  const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland'];
+  console.log(changeToUpperCase(countries));
+
+  const printFullName = (firstName, lastName) => `${firstName} ${lastName}`
+
+console.log(printFullName('Ömer', 'Tunç'))
